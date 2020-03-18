812ca458bfea3b57255debf66ed53e757307e991
Mohamed <fakharodine2015@gmail.com>
2020-03-17 21:16:57 -0400

    We add new package and then we create Day2 class ,which have if statements in it

---
+++ src/Class2/Day2.java
@@ -0,0 +1,15 @@
+package Class2;
+
+public class Day2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+boolean israinytoda = true;
+if(israinytoda) {
+	System.out.println("rainy");
+}
+		
+		
+	}
+
+}
